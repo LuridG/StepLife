07814ba87bb45b99544b9f88646e71697b09c7dc
Lurid <lurid_tt@outlook.com>
2026-07-30 11:45:40 +0800

    chore: generate native Android launcher icons and configure APK build settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.steplife.steplife"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +29,18 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    // 自动重命名输出 APK 为: StepLife-v1.1.0-arm64-v8a.apk 规范名称
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            val abiFilter = output.getFilter(com.android.build.OutputFile.ABI)
+            val abi = if (abiFilter.isNullOrEmpty()) "universal" else abiFilter
+            output.outputFileName = "StepLife-v${variant.versionName}-${abi}.apk"
         }
     }
 }
